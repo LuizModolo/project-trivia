4ab8005fd83360c5067496d0e6c50c106d8d0b3c
Luiz Fernando C. Modolo <modolo.luizfernando@gmail.com>
2021-11-29 15:27:01 -0300

    requisito 13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { playerData } = this.props;
+    const scoreResult = 3;
     return (
       <div>
         <Header />
-        <p data-testid="feedback-text">Oi</p>
+        { playerData.assertions >= scoreResult
+          ? <p data-testid="feedback-text">Mandou bem!</p>
+          : <p data-testid="feedback-text">Podia ser melhor...</p>}
       </div>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  playerData: state.headerReducer.player,
+});
+
+Feedback.propTypes = {
+  playerData: PropTypes.shape().isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
