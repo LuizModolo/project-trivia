bd9a4d23fea6eb51163817e1c17f10cbd920cb34
Luiz Fernando C. Modolo <modolo.luizfernando@gmail.com>
2021-11-29 15:41:37 -0300

    requisito 14 e 15

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Button from '../components/Button';
 
 class Feedback extends Component {
+  constructor() {
+    super();
+
+    this.handleStartAgain = this.handleStartAgain.bind(this);
+  }
+
+  handleStartAgain() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     const { playerData } = this.props;
     const scoreResult = 3;
@@ -13,6 +25,23 @@ class Feedback extends Component {
         { playerData.assertions >= scoreResult
           ? <p data-testid="feedback-text">Mandou bem!</p>
           : <p data-testid="feedback-text">Podia ser melhor...</p>}
+        <p>
+          Sua pontuação foi:
+          {' '}
+          <span data-testid="feedback-total-score">{ playerData.score }</span>
+        </p>
+        <p>
+          Você acertou
+          {' '}
+          <span data-testid="feedback-total-question">{ playerData.assertions }</span>
+          {' '}
+          questões!
+        </p>
+        <Button
+          testId="btn-play-again"
+          onClick={ this.handleStartAgain }
+          labelText="Jogar novamente"
+        />
       </div>
     );
   }
@@ -24,6 +53,7 @@ const mapStateToProps = (state) => ({
 
 Feedback.propTypes = {
   playerData: PropTypes.shape().isRequired,
+  history: PropTypes.shape().isRequired,
 };
 
 export default connect(mapStateToProps)(Feedback);
